7a679b8576dea1bfffe40628e55cff0f5c725256
Andrei Stan <stanfandrei@yahoo.com>
2025-07-24 19:10:48 +0300

    Update SMTP Settings V3

--- Aplicatie_Scanner/Frm_Dashboard.cs
+++ Aplicatie_Scanner/Frm_Dashboard.cs
@@ -1071,9 +1071,12 @@ namespace Azel_Raportare_Balkani
             ///////////////////////////////
             try
             {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                                      | SecurityProtocolType.Tls11
+                                      | SecurityProtocolType.Tls12;
                 var smtpClient = new SmtpClient("mail.azel.ro")
                 {
-                    Port = 465,
+                    Port = 587,
                     Credentials = new NetworkCredential("raportari_balkan@azel.ro", "-h.on^gdq+N;"),
                     EnableSsl = false,
                 };
